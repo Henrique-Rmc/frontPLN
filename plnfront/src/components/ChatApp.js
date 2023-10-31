1de3131504ec3e3e078bc7c2aea15e8a1fd796db
Henrique Ribeiro <49095666+Henrique-Rmc@users.noreply.github.com>
2023-10-31 12:05:24 -0300

    consumindo API

--- plnfront/src/components/ChatApp.js
+++ plnfront/src/components/ChatApp.js
@@ -1,10 +1,13 @@
-import React, { useState, Component } from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 function ChatApp() {
   const [message, setMessage] = useState("");
   const [status, setStatus] = useState("");
   const [loading, setLoading] = useState(false);
+  const [pdfData, setPdfData] = useState(null);
+
+  const url = 'https://example.com/api/generate-pdf';
 
   const sendMessage = () => {
     if (!message) {
@@ -13,45 +16,66 @@ function ChatApp() {
 
     setStatus("Carregando...");
     setLoading(true);
-
-    // Lógica de envio da mensagem para a API aqui
-
-    // Após receber a resposta da API, atualize o histórico
-    // com setHistory([...history, { userMessage: message, botMessage: response }])
-
-    setStatus("");
-    setMessage("");
-    setLoading(false);
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(message),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Erro ao solicitar PDF');
+        }
+        return response.blob();
+      })
+      .then((pdfBlob) => {
+        const pdfUrl = URL.createObjectURL(pdfBlob);
+        setPdfData(pdfUrl); // Atualize o estado pdfData
+      })
+      .catch((error) => {
+        console.error('Erro ao solicitar pdf', error);
+      })
+      .finally(() => {
+        setStatus("");
+        setMessage("");
+        setLoading(false);
+      });
   };
 
   return (
     <div className="body">
-
       <div className="header">
         <h1>I.A Para Otimização de Processos Jurídicos</h1>
       </div>
 
-          <p id="status">{status}</p>
-          <div className="entry">
-          <p>Nossa I.A tem a capacidade de receber um relato informal em um processo formal para otimizar seu tempo</p>
-          <textarea
-            type="text"
-            id="message-input"
-            placeholder="Digite aqui"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-          <button
-            className="btn-submit"
-            id="btn-submit"
-            onClick={sendMessage}
-            disabled={loading}
-          >
-            Gerar PDF
-          </button>
-          </div>
+      <p id="status">{status}</p>
+      <div className="entry">
+        <p>Nossa I.A tem a capacidade de receber um relato informal em um processo formal para otimizar seu tempo</p>
+        <textarea
+          type="text"
+          id="message-input"
+          placeholder="Digite aqui"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button
+          className="btn-submit"
+          id="btn-submit"
+          onClick={sendMessage}
+          disabled={loading}
+        >
+          Gerar PDF
+        </button>
+        <div>
+          {pdfData ? (
+            <embed src={pdfData} width="100%" height="500px" type="application/pdf" />
+          ) : (
+            <p>Carregando PDF...</p>
+          )}
+        </div>
       </div>
-
+    </div>
   );
 }
 
